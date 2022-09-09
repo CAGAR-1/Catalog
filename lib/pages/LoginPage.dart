a4398f7a7eeec89bf41cc8493675afb2d2266a8f
Sagar <Sagararyal1122@gmail.com>
2022-09-10 00:06:34 +0545

    Fixed Login

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -3,13 +3,23 @@ import 'package:ecommerce/utils/route.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final formKey = GlobalKey<FormState>();
+   LoginPage({super.key});
+
+  TextEditingController emailController = TextEditingController();
+
+  TextEditingController passwordController = TextEditingController();
+
+  String name = "";
+
+  bool change = false;
 
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: SingleChildScrollView(
+        child: Form(
+          key: formKey,
           child: Column(
             children: [
               Image.asset(
@@ -17,36 +27,59 @@ class LoginPage extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
               Text(
-                "Welcome",
+                "Welcome  " + name,
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(
                     vertical: 16.0, horizontal: 32.0),
-                child: Column(
-                  children: [
-                    TextFormField(
+                child: Column(children: [
+                  TextFormField(
+                      controller: emailController,
                       decoration: InputDecoration(
-                          hintText: "Enter UserName", labelText: "Username"),
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: "Enter Password", labelText: "Password"),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, route.homeRoute);
+                          hintText: "Enter Email", labelText: "Email"),
+                      onChanged: (value) {
+                        name = value;
+                       
                       },
-                      child: Text("Login"),
-                      
-                    ),
-                    
-                  ],
-                ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please Enter email';
+                        }
+                        if (value.contains("@") == false) {
+                          return 'Please enter valid email';
+                        }
+                        return null;
+                      }),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter password';
+                      }
+                      if (value.length < 6) {
+                        return 'Password should contain at least 6 charactere';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                    width: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        var isValid = formKey.currentState!.validate();
+                        
+                        if (isValid) {
+                          print(emailController.text);
+                        }
+                      },
+                      child: const Text("SignIn"))
+                ]),
               )
             ],
           ),
